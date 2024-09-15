8a6f255a4217c0c578083b79597bad1cb83f48da
krendel506 <krendel506@gmail.com>
2024-09-15 10:21:17 +0300

    added prototypes

--- homework_5.c
+++ homework_5.c
@@ -1,50 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
     float scores[5];
     float avg;
     float current;
-    double diff =0;
+    double diff = 0;
     double maxDiff = 0;
-    int index =0;
-  
+    int index = 0;
 
     printf("5 tasters rate a drink from 0 to 10\n");
 
-    for(int i =0; i<5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         printf("Enter %d taster score: ", i + 1);
-        scanf("%f",&current);
-        if(current >= 0 && current <= 10){
-        scores[i] = current;
-        avg = avg + current;
+        scanf("%f", &current);
+        if (current >= 0 && current <= 10)
+        {
+            scores[i] = current;
+            avg = avg + current;
         }
-        else{
+        else
+        {
             printf("Input has to be between 0 and 10\n");
             break;
         }
     }
-    
+
     avg = avg / 5;
 
-//find the index of score that differed the most from average
-    for(int a = 0; a < 5; a++){
-        
+    // find the index of score that differed the most from average
+    for (int a = 0; a < 5; a++)
+    {
+
         diff = scores[a] - avg;
         double res = fabs(diff);
 
-        if(res > maxDiff){
-            
+        if (res > maxDiff)
+        {
+
             maxDiff = diff;
             index = a + 1;
         }
     }
 
     printf("Average score: %.0f\n", avg);
-    if(index == 0){
+    if (index == 0)
+    {
         printf("All tasters scored the same\n");
     }
-    else{
+    else
+    {
         printf("Taster %d score differed the most from the average.\n", index);
     }
     return 0;
--- homeworks_4.c
+++ homeworks_4.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     long distance = 384400;
     long smallestV = 365180;
     long biggestV = 403620;
     long guess = 0;
 
-    while(1){
+    while (1)
+    {
         printf("Enter planet Earth distance from the Moon: ");
         scanf("%ld", &guess);
-        if(guess == 0){
+        if (guess == 0)
+        {
             break;
         }
-        else if(guess >= smallestV && guess <= biggestV){
+        else if (guess >= smallestV && guess <= biggestV)
+        {
             printf("Correct input\n");
             break;
         }
-        else if(guess < smallestV){
-            printf("The correct answer is bigger\n");  
+        else if (guess < smallestV)
+        {
+            printf("The correct answer is bigger\n");
         }
-        else if(guess > biggestV){
+        else if (guess > biggestV)
+        {
             printf("The correct value is smaller\n");
         }
     }
